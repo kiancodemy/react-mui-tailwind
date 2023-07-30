dc3fa038bd9964cfc48fed240501651bb46ca91d
kian kheirani <kiankheirani@gmail.com>
2023-07-30 23:10:00 +0430

    'add-header'

---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+import ResponsiveAppBar from "./appbar";
+export default function Header() {
+  return (
+    <div className="max-w-[1920px] max-h-[61px] py-[12px] px-[30px] gap-[8px] bg-[#FFFFFF] shadow-[#6463631A] shadow-[0_1px_6px_0px]">
+      <ResponsiveAppBar></ResponsiveAppBar>
+    </div>
+  );
+}
