38ca3c3456d133e5f25899a43f3622077526c3f2
kian kheirani <kiankheirani@gmail.com>
2023-08-02 10:51:20 +0430

    'final-3'

--- src/components/container.js
+++ src/components/container.js
@@ -31,7 +31,7 @@ export default function Container() {
             className={`${
               dark
                 ? "mb-[25px] max-w-[868px] max-h-[42px] pb-[10px] bg-[#FFFFFF]"
-                : "max-w-[868px] max-h-[42px] pb-[10px] bg-[#2B3139]"
+                : " max-w-[868px] max-h-[82px] pb-[10px] bg-[#2B3139]"
             }`}
           >
             <p>
@@ -39,15 +39,15 @@ export default function Container() {
                 onClick={() => {
                   setdark(!dark);
                 }}
-                className="md:ml-[25px]  ml-[110px]"
+                className="ml-[25px] "
                 {...label}
               />
             </p>
             <p
               className={`${
                 dark
-                  ? " font-roboto py-[10px] ml-[110px]   md:ml-[25px] text-[24px] max-w-[220px] max-h-[32px] text-[#474D57] leading-[32px] text-center tracking-normal"
-                  : "font-roboto py-[10px] ml-[25px] text-[24px] max-w-[220px] max-h-[32px] text-[#EAECEF] leading-[32px] text-center tracking-normal"
+                  ? "ml-[25px] font-roboto py-[10px]  text-[24px] max-w-[220px] max-h-[32px] text-[#474D57] leading-[32px]  text-center tracking-normal"
+                  : "ml-[25px] font-roboto py-[10px]  text-[24px] max-w-[220px] max-h-[32px] text-[#EAECEF] leading-[32px] text-center tracking-normal"
               }`}
             >
               Log into Iran Source
@@ -56,7 +56,7 @@ export default function Container() {
           <div
             className={`${
               dark
-                ? "bg-[#FFFFFF] flex items-center flex-col  max-w-[868px] max-h-[1380px] pb-[10px] md:items-center gap-[25px] md:flex-row "
+                ? "bg-[#FFFFFF] flex items-center flex-col   max-w-[868px] max-h-[1380px] pb-[10px] md:items-center gap-[25px] md:flex-row "
                 : "bg-[#2B3139] flex items-center flex-col  max-w-[868px] max-h-[1380px] pb-[10px] md:items-center gap-[25px] md:flex-row "
             }  `}
           >
